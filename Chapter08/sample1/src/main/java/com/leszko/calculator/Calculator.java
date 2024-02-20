8bb2bad6c660a4adab755426605099e1320e785e
TheDragonKing <113741351+chhunkim3@users.noreply.github.com>
2024-02-19 21:11:54 -0500

    Update Calculator.java

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -6,9 +6,20 @@ import org.springframework.stereotype.Service;
 /** Calculator for the Exercise 5.2 */
 @Service
 public class Calculator {
-
 	@Cacheable("sum")
 	public int sum(int a, int b) {
 		return a + b;
 	}
+}
+
+public class Example1 {
+  public final static int FIRST_CONSTANT1 = 10;
+  protected final static int SECOND_CONSTANT2 = 100;
+  final static int third_Constant3 = 1000; // violation 'must match pattern'
+  private final static int fourth_Const4 = 50; // violation 'must match pattern'
+  public final static int log = 10; // violation 'must match pattern'
+  protected final static int logger = 50; // violation 'must match pattern'
+  final static int loggerMYSELF = 5; // violation 'must match pattern'
+  final static int MYSELF = 100;
+  protected final static int myselfConstant = 1; // violation 'must match pattern'
 }
