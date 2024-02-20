75719adcd5b0082e03c2250d041cbc3c6eadfd74
TheDragonKing <113741351+chhunkim3@users.noreply.github.com>
2024-02-19 22:07:18 -0500

    Update ConstantName.java

--- Chapter08/sample1/src/main/java/com/leszko/calculator/ConstantName.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/ConstantName.java
@@ -1,6 +1,7 @@
 package com.leszko.calculator;
 
-import org.springframework.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /** ConstantName class declaration */
 
