ada7265e31865ed8404f12d1c07b3fa8f94806c6
TheDragonKing <113741351+chhunkim3@users.noreply.github.com>
2024-02-19 21:46:35 -0500

    Create ConstantName.java

---
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/ConstantName.java
@@ -0,0 +1,13 @@
+/** ConstantName class declaration */
+
+public class ConstantName {
+  public final static int FIRST_CONSTANT1 = 10;
+  protected final static int SECOND_CONSTANT2 = 100;
+  final static int third_Constant3 = 1000; // violation 'must match pattern'
+  private final static int fourth_Const4 = 50; // violation 'must match pattern'
+  public final static int log = 10; // violation 'must match pattern'
+  protected final static int logger = 50; // violation 'must match pattern'
+  final static int loggerMYSELF = 5; // violation 'must match pattern'
+  final static int MYSELF = 100;
+  protected final static int myselfConstant = 1; // violation 'must match pattern'
+}
