48f8f1aa69957e8c8dbb0aec112845183caf4db7
TheDragonKing <113741351+chhunkim3@users.noreply.github.com>
2024-02-19 20:28:59 -0500

    Update Calculator.java

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,7 +3,6 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-/** Calculator for the Exercise 5.2 */
 @Service
 public class Calculator {
 
