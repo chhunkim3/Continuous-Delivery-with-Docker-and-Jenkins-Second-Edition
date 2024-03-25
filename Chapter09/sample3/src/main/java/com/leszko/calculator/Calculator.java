4ec7f89dea41fd6b6f98cf72250cf2cb9ea72bbc
TheDragonKing <113741351+chhunkim3@users.noreply.github.com>
2024-03-25 03:00:11 -0400

    Update Calculator.java

--- Chapter09/sample3/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter09/sample3/src/main/java/com/leszko/calculator/Calculator.java
@@ -6,14 +6,17 @@ import org.springframework.stereotype.Service;
 /** Calculator logic */
 @Service
 public class Calculator {
-		@Cacheable("sum")
-		public int sum(int a, int b) {
-			return a + b;
-		}
+	@Cacheable("sum")
+	public int sum(int a, int b) {
+		return a + b;
+	}
 
         @Cacheable("div")
         public int div(int a, int b) {
-                return a / b;
+		 if (b == 0) { 
+			throw new IllegalArgumentException("Denominator CANNOT be 0"); 
+		}
+                else return a / b;
 		}
 	}
 }
