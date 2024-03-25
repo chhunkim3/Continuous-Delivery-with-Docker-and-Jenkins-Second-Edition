e18f49b5dfcdefd1e7ba87b5e2c68d94bacd0e20
TheDragonKing <113741351+chhunkim3@users.noreply.github.com>
2024-03-25 02:49:31 -0400

    Update CalculatorController.java

--- Chapter09/sample3/src/main/java/com/leszko/calculator/CalculatorController.java
+++ Chapter09/sample3/src/main/java/com/leszko/calculator/CalculatorController.java
@@ -27,7 +27,7 @@ class CalculatorController {
         @RequestMapping("/div")
         String div(@RequestParam("a") Integer a,
                         @RequestParam("b") Integer b) {
-                String result = b!=0 ? String.valueOf(calculator.div(a, b)) : "Division by 0";
+                String result = b!=0 ? String.valueOf(calculator.div(a, b)) : "Failed due to Division by 0" return false;
                 calculationRepository.save(new Calculation(a.toString(), b.toString(), result, Timestamp.from(Instant.now())));
                 return result;
         }
