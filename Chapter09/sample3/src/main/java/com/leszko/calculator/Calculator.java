70c37ef6138257d8d562e9528a85220c1cb3f624
TheDragonKing <113741351+chhunkim3@users.noreply.github.com>
2024-03-25 02:40:27 -0400

    Update Calculator.java

--- Chapter09/sample3/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter09/sample3/src/main/java/com/leszko/calculator/Calculator.java
@@ -2,23 +2,18 @@ package com.leszko.calculator;
 
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
-import java.io.*
 
 /** Calculator logic */
 @Service
 public class Calculator {
-	@Cacheable("sum")
-	public int sum(int a, int b) {
-		return a + b;
-	}
+		@Cacheable("sum")
+		public int sum(int a, int b) {
+			return a + b;
+		}
 
         @Cacheable("div")
         public int div(int a, int b) {
-		If (b = 0){
-			System.out.println("Error: Denominator CANNOT be 0");
+                return a / b;
 		}
-		Else{
-                	return a / b;
-		}
-        }
+	}
 }
